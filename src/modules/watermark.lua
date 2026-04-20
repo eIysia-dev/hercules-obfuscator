44f8118db6c4efb2f2ec9fa97492b733ffdacd08
eIysia-dev <nnedyx19@gmail.com>
2026-04-20 23:12:01 +0800

    Update watermark.lua

--- src/modules/watermark.lua
+++ src/modules/watermark.lua
@@ -1,8 +1,16 @@
--- modules/watermark.lua
 local Watermark = {}
 
 function Watermark.process(code)
-    return "--[Obfuscated by Hercules v1.6.2 | hercules-obfuscator.xyz/discord | hercules-obfuscator.xyz/source]\n" .. code
+    -- Visible header comment
+    local header = "--[elysium was here :) | discord.gg/Elys1um]\n"
+    -- Hidden watermark: embedded as an unreachable dead string literal
+    -- that will appear in the bytecode constants table, making it
+    -- easy to identify the tool while being invisible at runtime
+    local hidden = string.format(
+        "do local _ = '%s' end\n",
+        "elysium_was_here_" .. tostring(math.random(100000, 999999))
+    )
+    return header .. hidden .. code
 end
 
 return Watermark
