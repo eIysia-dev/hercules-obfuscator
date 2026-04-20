1246c840be66452ba071f3e77bffb7d00166c7f4
eIysia-dev <nnedyx19@gmail.com>
2026-04-20 23:49:37 +0800

    Update VMGenerator.lua

--- src/modules/VMGenerator.lua
+++ src/modules/VMGenerator.lua
@@ -75,7 +75,7 @@ local function encode(str_param, yes)
 
     return table.concat(out)
 end
-	add("hercules,v1,alpha,__,_ = 'Protected By Hercules V1.6 | github.com/zeusssz/hercules-obfuscator', function()end, true, 1, 0")
+	add("elysium,v1,alpha,__,_ = 'elysium was here! :) | discord.gg/elys1um', function()end, true, 1, 0")
 	add(Parts.Variables)
 	add(Parts.Deserializer)
 	add(Parts.Wrapper_1)
