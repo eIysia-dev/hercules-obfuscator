ac3bd1d63b38b7cce598c9883c4052102563480d
eIysia-dev <nnedyx19@gmail.com>
2026-04-21 00:27:07 +0800

    Update WrapInFunction.lua

--- src/modules/WrapInFunction.lua
+++ src/modules/WrapInFunction.lua
@@ -1,5 +1,7 @@
 local Wrapper = {}
+
 function Wrapper.process(code)
-return [[return (function(...) ]]..code..[[ end)(...)]]
+    return "local args={...} return (function(...) " .. code .. " end)(table.unpack(args))"
 end
+
 return Wrapper
