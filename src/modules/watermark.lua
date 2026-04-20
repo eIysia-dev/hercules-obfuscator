19a9b8cc8d88a5df2c039373c2eadf4cd232fc32
eIysia-dev <nnedyx19@gmail.com>
2026-04-20 23:44:29 +0800

    Update watermark.lua

--- src/modules/watermark.lua
+++ src/modules/watermark.lua
@@ -1,16 +1,8 @@
+-- modules/watermark.lua
 local Watermark = {}
 
 function Watermark.process(code)
-    -- Visible header comment
-    local header = "--[elysium was here :) | discord.gg/Elys1um]\n"
-    -- Hidden watermark: embedded as an unreachable dead string literal
-    -- that will appear in the bytecode constants table, making it
-    -- easy to identify the tool while being invisible at runtime
-    local hidden = string.format(
-        "do local _ = '%s' end\n",
-        "elysium_was_here_" .. tostring(math.random(100000, 999999))
-    )
-    return header .. hidden .. code
+    return "--[elysium was here! | discord.gg/Elys1um]\n" .. code
 end
 
 return Watermark
